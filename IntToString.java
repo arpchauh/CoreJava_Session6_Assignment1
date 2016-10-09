61f4561ba2f3c4e2a8fc262d15d36d6e478660f7
arpchauh <arpitchauhan10@gmail.com>
2016-10-09 13:14:14 +0530

    Add files via upload

---
+++ IntToString.java
@@ -0,0 +1,16 @@
+package com.acadgild.java.session6;
+public class IntToString {
+
+	public static void main(String[] args) {
+		
+		int i =50; //Initializing the integer variable i
+		
+		System.out.println("This vaiable is integer :"+i);
+		
+		String s = Integer.toString(i); //converting the integer to string 
+		
+		System.out.println("This variable is String :"+s);
+
+	}
+
+}
